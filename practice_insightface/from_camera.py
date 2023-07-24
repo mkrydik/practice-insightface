f4eeab8e8087478cf7328b14edb80ab40632cf23
mkrydik <mikuriya.d@opst.co.jp>
2023-07-24 14:16:00 +0900

    Update

--- practice_insightface/from_camera.py
+++ practice_insightface/from_camera.py
@@ -15,12 +15,17 @@ cap = cv2.VideoCapture(camera_id)
 if not cap.isOpened():
     sys.exit()
 
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('H', '2', '6', '4'))
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+
 while True:
     ret, frame = cap.read()
     if ret:
         faces = app.get(np.asarray(frame))
-        print('')
-        print(faces)  # gender・age プロパティが存在する・その他は特徴点の座標値と思われる
+        #print('')
+        #print(faces)  # gender・age プロパティが存在する・その他は特徴点の座標値と思われる
         rimg = app.draw_on(frame, faces)
         
         cv2.imshow(window_name, rimg)
